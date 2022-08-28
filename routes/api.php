7b5b9c330216dced16d745f81e027e7b12ac613b
Tolu Soneye <sammyjayden27@gmail.com>
2022-08-28 23:21:11 +0000

    wrote my first get api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+// Created my first api
+Route::get('/posts', function (){
+    return response()->json([
+        'posts' => [
+            [
+                'title' => 'My Post',
+            ]
+        ]
+    ]);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
