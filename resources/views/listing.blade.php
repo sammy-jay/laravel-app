2039f950a9ee9d0bdbbe8667886413d9c7e0def1
Tolu Soneye <sammyjayden27@gmail.com>
2022-08-29 00:09:05 +0000

    added more blade templates

---
+++ resources/views/listing.blade.php
@@ -0,0 +1,5 @@
+<h1 >Single Listing</h1>
+
+
+<h2>{{ $listing['title'] }}</h2>
+<p>{{ $listing['description'] }}</p>
--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,15 +1,5 @@
 <h1 >{{ $heading }}</h1>
 
-@unless (count($listings) == 0)
-     @foreach($listings as $listing)
-        <h2>{{ $listing['title'] }}</h2>
-        <p>{{ $listing['description'] }}</p>
-    @endforeach
-@else
-     <h2>No Listings</h2>
-@endunless
-
-
 @if (count($listings) == 0)
     <h2>No Listings</h2>
 @else
@@ -18,10 +8,3 @@
         <p>{{ $listing['description'] }}</p>
     @endforeach
 @endif
-
-
-
-@php
-    $test = 1;
-@endphp
-{{$test}}
